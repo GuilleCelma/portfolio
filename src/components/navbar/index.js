e9da76e899cddfafaec494b74853d3f21f2bd394
Guille Celma <guillecelmaan@hotmail.es>
2021-10-13 00:20:00 +0200

    Feature: added nav bar color change on scroll

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,9 +6,20 @@ import React, {useState} from "react"
 
 const Navbar = () =>{
     const[showLinks, setShowLinks] = useState(false)
+    const[navColor, setNavColor] = useState(false)
+
+    const changeNavBackground = () =>{
+        if(window.scrollY >= 80){
+            setNavColor(true)
+        }else{
+            setNavColor(false)
+        }
+    }
+
+    window.addEventListener("scroll", changeNavBackground)
 
     return (
-        <div className="Navbar">
+        <nav className={ navColor ? "Navbar active" : "Navbar"} >
             <div className="leftSide">
             <h1>Logo</h1>
             </div>
@@ -18,7 +29,7 @@ const Navbar = () =>{
                 <button onClick={()=> setShowLinks(!showLinks)
                 }><img src="/menu.png" alt="" id ="menuIcon"></img></button>
             </div>
-        </div>
+        </nav>
     )
 }
 
