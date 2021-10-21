b168ac5683b37a0086c4eaf17d759f571be3573a
Guille Celma <guillecelmaan@hotmail.es>
2021-10-21 19:11:49 +0200

    fix: fixing some responsive errors in menu 5

--- src/components/navbar/NavbarElemenets.js
+++ src/components/navbar/NavbarElemenets.js
@@ -6,17 +6,21 @@ import { Link } from "react-scroll"
 
    
     return(
-        <ul id={id}>
-            <li>
-                <Link to="about" smooth={true} duration={1000} className={color}>About</Link>
-            </li>
-            <li>
-                <Link to="work" smooth={true} duration={1000} className={color}>Work</Link>
-            </li>
-            <li>
-                <Link to="contact" smooth={true} duration={1000} className={color}>Contact</Link>
-            </li>
-        </ul>
+        <div className={id}>
+
+            <ul className={id}>
+                <li>
+                    <Link to="about" smooth={true} duration={1000} className={color}>About</Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true} duration={1000} className={color}>Work</Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={1000} className={color}>Contact</Link>
+                </li>
+            </ul>
+
+        </div>
     )
 }
 
