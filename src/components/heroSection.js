1f1d2a6a44c046a3d1bcabec31ebdee68fd68282
Guille Celma <guillecelmaan@hotmail.es>
2021-10-23 23:38:10 +0200

    fix: adjusted about heigh

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -6,7 +6,7 @@ const HeroSection = ()=> {
         <section id="hero">
             <div id="hero-container">
                 <h3>Welcome!</h3>
-                <h1> I'm Guille Celma, Web Developer</h1>
+                <h1> I'm Guille Celma,Web Developer</h1>
                 <a href="CV-EN.pdf" download="GFG" id="downloadBtn">Download CV</a>
             </div>
         </section>
